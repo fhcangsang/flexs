61e5f5d50113de6866c015af4149384039bffbff
王旺旺 <43838223+wang-wan-wa@users.noreply.github.com>
2018-10-24 15:26:52 +0800

    Update wx_itfun/pages/more/more.js

---
+++ wx_itfun/pages/more/more.js
@@ -0,0 +1,16 @@
+// pages/more/more.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    teacher: "https://itfun.tv/teachers",
+    about:"https://itfun.tv/about",
+    member:"https://itfun.tv/premiums",
+    problem:"https://itfun.tv/trains#faq"
+  },
+  onLoad: function (options) {
+
+  }
+})
