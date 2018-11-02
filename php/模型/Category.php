d7d56bdacdfb172bbf6010bc30f9b09e3df5f988
王旺旺 <43838223+wang-wan-wa@users.noreply.github.com>
2018-11-02 11:06:56 +0800

    Update php/模型/Category.php

--- php/模型/Category.php
+++ php/模型/Category.php
@@ -1,10 +1,26 @@
 <?php
 
-namespace App;
+namespace App\Models\Shop;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    //每个一级分类有很多二级分类
+    public function children()
+    {
+        return $this->hasMany('App\Models\Shop\Category', 'parent_id', 'id');
+    }
+
+
+    static function all_categories()
+    {
+        $categories = self::where('parent_id', 0)->get();
+        foreach ($categories as $key => $value) {
+            $id = $value['id'];
+            $children = self::where('parent_id', $id)->get();
+            $categories[$key]['children'] = $children;
+        }
+        return $categories;
+    }
 }
